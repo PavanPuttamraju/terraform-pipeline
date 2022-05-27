9f1e738af5a0a1dd4c5a062f27688db4f77184cf
Pavan Puttamraju <pavan.puttamraju@ad.infosys.com>
2022-05-27 12:28:34 +0530

    Adding GCS bucket code

---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "project_id" {
+description = "Project ID where GCP bucket will be provisioned"
+}
+
+variable "bucket_name" {
+description = "GCS Bucket name. Value should be unique ."
+type        = string
+}
+
+variable "bucket_location" {
+description = "location of the bucket"
+type        = string
+default     = "US"
+}
+
+variable "bucket_storage_class" {
+description = "type of storage class for gcs bucket"
+default     = "standard"
+}
+
+variable "bucket_force_destroy" {
+type = map(bool)
+default     = {
+    "first" = false
+}
+}
+
+variable "bucket_versioning" {
+type = map(bool)
+default     = {
+    "first" = true
+}
+}
+
+
+
+
+
+
+
+
