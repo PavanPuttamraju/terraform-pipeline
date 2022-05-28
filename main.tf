a708f223d98828efab94ed961a00b2f1f9b12fc9
PavanPuttamraju <100206143+PavanPuttamraju@users.noreply.github.com>
2022-05-28 17:36:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 module "gcs_buckets" {
   source  = "terraform-google-modules/cloud-storage/google"
-  project_id  = var.project_id 
   names = var.bucket_name
   prefix = ""
   storage_class = var.bucket_storage_class
